b0a463e6d3aa3eed5e7edaa354e63fdf2790bcc3
Arturo De Jesus <adejesus.est@gmail.com>
2020-06-26 18:12:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/headsup/splash.java
@@ -0,0 +1,43 @@
+package com.example.headsup;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splash extends AppCompatActivity {
+    private TextView txtV;
+    private ImageView imgV;
+    //Esto es el splash screen, inicializando y programan transicion
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        txtV = (TextView) findViewById(R.id.splash_txt);
+        imgV = (ImageView) findViewById(R.id.splash_logo);
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
+        txtV.startAnimation(myanim);
+        imgV.startAnimation(myanim);
+        final Intent i = new Intent(this, MainActivity.class);
+        Thread timer = new Thread(){
+            public void run (){
+                try {
+                    sleep(5000);
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                //transicion del splash screen al primer activity
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+            timer.start();
+    }
+}
